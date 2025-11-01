ce566324a6e71b18f0d701d878a65c8b0cfbaab6
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2025-11-01 16:35:44 +0100

    Update read_canum_sheet_old_format_IIIa.R

--- utils/read_canum_sheet_old_format_IIIa.R
+++ utils/read_canum_sheet_old_format_IIIa.R
@@ -1,6 +1,6 @@
 
+
 read_canum_sheet_old_format_IIIa <- function(file, sheet_name) {
-  
   library(readxl)
   library(stringr)
   library(dplyr)
@@ -8,7 +8,8 @@ read_canum_sheet_old_format_IIIa <- function(file, sheet_name) {
   dat <- read_excel(path = file, sheet = sheet_name)
   
   # Read header informations
-  position_head <- which(apply(dat, 1, function(x) any(grepl("Division:", x))))
+  position_head <- which(apply(dat, 1, function(x)
+    any(grepl("Division:", x))))
   
   head <- dat[position_head, ]
   
@@ -37,14 +38,14 @@ read_canum_sheet_old_format_IIIa <- function(file, sheet_name) {
   
   
   # Canum
-  position_quarter <- which(apply(dat, 1, function(x) any(grepl("Quarter", x))))
+  position_quarter <- which(apply(dat, 1, function(x)
+    any(grepl("Quarter", x))))
   
   canum <- c()
   
   for (i in position_quarter[1:4]) {
-    
     # Per Quarter
-    q <- dat[c((i-1):(i+11)), ]
+    q <- dat[c((i - 1):(i + 11)), ]
     
     head_col <- q[c(1:nrow(q)), c(1:2)]
     
@@ -53,27 +54,37 @@ read_canum_sheet_old_format_IIIa <- function(file, sheet_name) {
     ## Per fleet
     position_fleet <- grep("Fleet", q)
     
-    for (j in position_fleet) {
-      
-      fleet <- q[, c(j:(j+1))]  
+    for (j in position_fleet[c(1:3)]) { 
+      fleet <- q[, c(j:(j + 1))]
       
       fleet_1 <- cbind(head_col, fleet)
       
       
-      fleet_name <- gsub(".*\\s+","", as.character(fleet[1, 1]))
+      fleet_name <- gsub(".*\\s+", "", as.character(fleet[1, 1]))
       fleet_1$quarter <- as.numeric(fleet_1$quarter)
       quarter <- subset(fleet_1, !(is.na(quarter)))
       quarter <- quarter[, 1]
       catch_t <- subset(fleet_1, wr == "SOP")
       catch_t <- as.numeric(catch_t[, 4])
-      wr <- as.character(fleet_1[c(3:10), 2])
+      if (quarter %in% c(1, 2)) {
+        wr <- as.character(fleet_1[c(3:10), 2])
+        fleet_dat <- fleet_1[c(3:10), c(3:4)]
+      } else {
+        wr <- as.character(fleet_1[c(3:11), 2])
+        fleet_dat <- fleet_1[c(3:11), c(3:4)]
+      }
       
-      fleet_dat <- fleet_1[c(3:10), c(3:4)]
+      
       colnames(fleet_dat) <- c("canum", "weca")
       
       #Combine
-      canum_qf <- mutate(fleet_dat, wr = wr, fleet = fleet_name, quarter = quarter,
-                         catch_t = catch_t)
+      canum_qf <- mutate(
+        fleet_dat,
+        wr = wr,
+        fleet = fleet_name,
+        quarter = quarter,
+        catch_t = catch_t
+      )
       
       canum <- rbind(canum, canum_qf)
     }

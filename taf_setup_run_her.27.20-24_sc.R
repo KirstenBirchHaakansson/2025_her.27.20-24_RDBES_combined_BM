4e41a692811d34270f7c4b8b4fdd376ca64ed992
Kirsten Birch Håkansson, DTU Aqua <kih@aqua.dtu.dk>
2025-11-01 15:59:26 +0100

    Create taf_setup_run_her.27.20-24_sc.R

---
+++ taf_setup_run_her.27.20-24_sc.R
@@ -0,0 +1,56 @@
+
+library(icesTAF)
+
+getwd()
+# taf.skeleton()
+
+# Make yearly folders for historical data
+
+
+# draft.data(data.files = "preliminary_catch_statistics",
+#            data.scripts = NULL,
+#            originator = "ICES",
+#            title = "Preliminary catch statistic from ICES",
+#            file = T,
+#            append = F)
+
+draft.data(data.files = "historical_data",
+           data.scripts = NULL,
+           originator = "Data from previous AWG's",
+           title = "Data from previous AWG's",
+           period = "2000-2015",
+           access = "Restricted",
+           file = T,
+           append = F)
+
+draft.data(data.files = "data_from_recent_years",
+           data.scripts = NULL,
+           originator = "Data from previous AWG's",
+           title = "Data from previous AWG's",
+           period = "2016-2024",
+           access = "Restricted",
+           file = T,
+           append = T)
+
+draft.data(data.files = "updated_data_from_sweden",
+           data.scripts = NULL,
+           originator = "Data from previous AWG's",
+           title = "Data from previous AWG's",
+           period = "2016-2024",
+           access = "Restricted",
+           file = T,
+           append = T)
+
+
+
+
+
+taf.boot()
+
+# mkdir("data")
+# mkdir("output")
+
+# sourceTAF("data") 
+# 
+# sourceTAF("report") 
+# run model_0....
